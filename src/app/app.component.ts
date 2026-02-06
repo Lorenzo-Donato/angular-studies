8282261d129578db46a32fda7efe053d21cb05c0
Lorenzo Donato <lorenzodonato75@gmail.com>
2026-02-06 15:14:46 -0300

    feat: added signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from './user/user.component';
 
-
 @Component({
   selector: 'app-root',
   standalone: true,
