42df43999c2eef86f96d7fd1cd405b33d741b3a1
Lorenzo Donato <lorenzodonato75@gmail.com>
2026-02-06 16:32:29 -0300

    chore: fixed tasks

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,4 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
 
 
 @Component({
@@ -13,7 +12,7 @@ export class UserComponent {
   @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
-  @Output() select = new EventEmitter();
+  @Output() select = new EventEmitter<string>();
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
